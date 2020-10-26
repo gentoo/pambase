gentoo/pambase

commit eb138196aa2d3cb860d5eb5ab1d05985df34ad2c

templates/system-auth.tpl: use faillock in minimal case
Bug: https://bugs.gentoo.org/748405
Signed-off-by: Sam James <sam@gentoo.org>

templates/system-auth.tpl
@@ -7,13 +7,10 @@ auth		sufficient	pam_ssh.so
 auth        [success=1 default=ignore]      pam_krb5.so {{ krb5_params }}
 {% endif %}
 
-auth		required	pam_unix.so try_first_pass {{ likeauth }} {{ nullok|default('', true) }} {{ debug|default('', true) }}
 auth		optional	pam_permit.so
-{% if not minimal %}
-auth		required	pam_faillock.so preauth
-auth		sufficient	pam_unix.so nullok try_first_pass
+auth		requisite	pam_faillock.so preauth
+auth		[success=1 default=ignore]	pam_unix.so {{ nullok|default('', true) }} {{ debug|default('', true) }} try_first_pass
 auth		[default=die]	pam_faillock.so authfail
-{% endif %}
 
 {% if krb5 %}
 account		[success=1 default=ignore]	pam_krb5.so {{ krb5_params }}

commit 473b931a56c9387cc6a1e1eddef2260fc9f3896f

templates/system-login.tpl: always need faillock
Fixes: eb138196aa2d3cb860d5eb5ab1d05985df34ad2c
Signed-off-by: Sam James <sam@gentoo.org>

templates/system-auth.tpl
@@ -17,9 +17,7 @@ account		[success=1 default=ignore]	pam_krb5.so {{ krb5_params }}
 {% endif %}
 account		required	pam_unix.so {{ debug|default('', true) }}
 account		optional	pam_permit.so
-{% if not minimal %}
 account         required        pam_faillock.so
-{% endif %}
 
 {% if passwdqc %}
 password	required	pam_passwdqc.so config=/etc/security/passwdqc.conf

templates/system-login.tpl
@@ -5,9 +5,7 @@ auth		include		system-auth
 account		required	pam_access.so {{ debug|default('', true) }}
 account		required	pam_nologin.so
 account		include		system-auth
-{% if not minimal %}
 account         required        pam_faillock.so
-{% endif %}
 
 password	include		system-auth
 session         optional        pam_loginuid.so