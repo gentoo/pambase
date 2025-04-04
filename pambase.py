66741bf54d420197a5fd18ee7720f452aa23389a
Mike Gilbert <floppym@gentoo.org>
2025-04-04 14:11:35 -0400

    Run black on pambase.py
    Signed-off-by: Mike Gilbert <floppym@gentoo.org>

--- pambase.py
+++ pambase.py
@@ -6,28 +6,72 @@ import pathlib
 
 
 def main():
-    parser = argparse.ArgumentParser(description='basic Gentoo PAM configuration files')
-    parser.add_argument('--gnome-keyring', action="store_true", help='enable pam_gnome_keyring.so module')
-    parser.add_argument('--caps', action="store_true", help='enable pam_cap.so module')
-    parser.add_argument('--passwdqc', action="store_true", help='enable pam_passwdqc.so module')
-    parser.add_argument('--pwhistory', action="store_true", help='enable pam_pwhistory.so module')
-    parser.add_argument('--pwquality', action="store_true", help='enable pam_pwquality.so module')
-    parser.add_argument('--openrc', action="store_true", help='enable pam_openrc.so module')
-    parser.add_argument('--elogind', action="store_true", help='enable pam_elogind.so module')
-    parser.add_argument('--systemd', action="store_true", help='enable pam_systemd.so module')
-    parser.add_argument('--homed', action="store_true", help='enable pam_systemd_home.so module')
-    parser.add_argument('--selinux', action="store_true", help='enable pam_selinux.so module')
-    parser.add_argument('--mktemp', action="store_true", help='enable pam_mktemp.so module')
-    parser.add_argument('--pam-ssh', action="store_true", help='enable pam_ssh.so module')
-    parser.add_argument('--securetty', action="store_true", help='enable pam_securetty.so module')
-    parser.add_argument('--shells', action="store_true", help='enable pam_shells.so module')
-    parser.add_argument('--sssd', action="store_true", help='enable sssd.so module')
-    parser.add_argument('--yescrypt', action="store_true", help='enable yescrypt option for pam_unix.so module')
-    parser.add_argument('--sha512', action="store_true", help='enable sha512 option for pam_unix.so module')
-    parser.add_argument('--krb5', action="store_true", help='enable pam_krb5.so module')
-    parser.add_argument('--minimal', action="store_true", help='install minimalistic PAM stack')
-    parser.add_argument('--debug', action="store_true", help='enable debug for selected modules')
-    parser.add_argument('--nullok', action="store_true", help='enable nullok option for pam_unix.so module')
+    parser = argparse.ArgumentParser(description="basic Gentoo PAM configuration files")
+    parser.add_argument(
+        "--gnome-keyring",
+        action="store_true",
+        help="enable pam_gnome_keyring.so module",
+    )
+    parser.add_argument("--caps", action="store_true", help="enable pam_cap.so module")
+    parser.add_argument(
+        "--passwdqc", action="store_true", help="enable pam_passwdqc.so module"
+    )
+    parser.add_argument(
+        "--pwhistory", action="store_true", help="enable pam_pwhistory.so module"
+    )
+    parser.add_argument(
+        "--pwquality", action="store_true", help="enable pam_pwquality.so module"
+    )
+    parser.add_argument(
+        "--openrc", action="store_true", help="enable pam_openrc.so module"
+    )
+    parser.add_argument(
+        "--elogind", action="store_true", help="enable pam_elogind.so module"
+    )
+    parser.add_argument(
+        "--systemd", action="store_true", help="enable pam_systemd.so module"
+    )
+    parser.add_argument(
+        "--homed", action="store_true", help="enable pam_systemd_home.so module"
+    )
+    parser.add_argument(
+        "--selinux", action="store_true", help="enable pam_selinux.so module"
+    )
+    parser.add_argument(
+        "--mktemp", action="store_true", help="enable pam_mktemp.so module"
+    )
+    parser.add_argument(
+        "--pam-ssh", action="store_true", help="enable pam_ssh.so module"
+    )
+    parser.add_argument(
+        "--securetty", action="store_true", help="enable pam_securetty.so module"
+    )
+    parser.add_argument(
+        "--shells", action="store_true", help="enable pam_shells.so module"
+    )
+    parser.add_argument("--sssd", action="store_true", help="enable sssd.so module")
+    parser.add_argument(
+        "--yescrypt",
+        action="store_true",
+        help="enable yescrypt option for pam_unix.so module",
+    )
+    parser.add_argument(
+        "--sha512",
+        action="store_true",
+        help="enable sha512 option for pam_unix.so module",
+    )
+    parser.add_argument("--krb5", action="store_true", help="enable pam_krb5.so module")
+    parser.add_argument(
+        "--minimal", action="store_true", help="install minimalistic PAM stack"
+    )
+    parser.add_argument(
+        "--debug", action="store_true", help="enable debug for selected modules"
+    )
+    parser.add_argument(
+        "--nullok",
+        action="store_true",
+        help="enable nullok option for pam_unix.so module",
+    )
 
     parsed_args = parser.parse_args()
     processed = process_args(parsed_args)
@@ -45,7 +89,7 @@ def process_args(args):
         "unix_extended_encryption",
         "likeauth",
         "nullok",
-        "local_users_only"
+        "local_users_only",
     ]
 
     # create a blank dictionary
@@ -80,8 +124,10 @@ def process_args(args):
 
 
 def parse_templates(processed_args):
-    load = FileSystemLoader('')
-    env = Environment(loader=load, trim_blocks=True, lstrip_blocks=True, keep_trailing_newline=True)
+    load = FileSystemLoader("")
+    env = Environment(
+        loader=load, trim_blocks=True, lstrip_blocks=True, keep_trailing_newline=True
+    )
 
     templates = [
         "login",
@@ -92,13 +138,13 @@ def parse_templates(processed_args):
         "su",
         "system-auth",
         "system-login",
-        "system-services"
+        "system-services",
     ]
 
     for template_name in templates:
-        template = env.get_template('templates/{0}.tpl'.format(template_name))
+        template = env.get_template("templates/{0}.tpl".format(template_name))
 
-        with open('stack/{0}'.format(template_name), "w+") as output:
+        with open("stack/{0}".format(template_name), "w+") as output:
             rendered_template = template.render(processed_args)
 
             # Strip all intermediate lines to not worry about appeasing Jinja
