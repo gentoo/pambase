gentoo/pambase

commit 74cbdaf50983f1eb06f82bdb5fbe7f762b859dd3

Add pam_gnome_keybase to auth, session stacks too
Quoting the bug:
> pam_gnome_keyring.so is, according to its own documentation [0], an authentication module,
> a session module, and a password management module. The expected way to use this module,
> AFAICT, is to place it in all three stacks, with the auto_start command line parameter
> present in the session line. In this configuration, the auth stack will hold onto the
> user’s passphrase, the session stack will launch a gnome-keyring-daemon process and hand
> over the passphrase to unlock the login keyring, and the password stack will change the
> encryption password for the login keyring when the user changes their system password.

[0] https://wiki.gnome.org/Projects/GnomeKeyring/Pam/Manual

Closes: https://github.com/gentoo/pambase/issues/8
Signed-off-by: Sam James <sam@gentoo.org>

templates/passwd.tpl
@@ -2,7 +2,3 @@ auth		sufficient	pam_rootok.so
 auth		include		system-auth
 account		include		system-auth
 password	include		system-auth
-
-{% if gnome_keyring %}
-password	optional	pam_gnome_keyring.so use_authtok
-{% endif %}

templates/system-local-login.tpl
@@ -1,4 +1,13 @@
 auth		include		system-login
+{% if gnome_keyring %}
+auth		optional	pam_gnome_keyring.so use_authtok
+{% endif %}
 account		include		system-login
 password	include		system-login
+{% if gnome_keyring %}
+password	optional	pam_gnome_keyring.so use_authtok
+{% endif %}
 session		include		system-login
+{% if gnome_keyring %}
+session		optional	pam_gnome_keyring.so use_authtok auto_start
+{% endif %}

commit 72e814a219cbed50155c65432f5389c82d73df96

Keep pam_gnome_keyring in passwd too
We want to update pam_gnome_keyring's record of the user's password
so it can be decrypted.

Fixes: 74cbdaf50983f1eb06f82bdb5fbe7f762b859dd3
Bug: https://github.com/gentoo/pambase/issues/8
Signed-off-by: Sam James <sam@gentoo.org>

templates/passwd.tpl
@@ -2,3 +2,7 @@ auth		sufficient	pam_rootok.so
 auth		include		system-auth
 account		include		system-auth
 password	include		system-auth
+
+{% if gnome_keyring %}
+password	optional	pam_gnome_keyring.so use_authtok
+{% endif %}